139174f6d887fb70bb9451f01aabc7ffbe42473b
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2019-10-23 16:15:06 +0800

    更换硬件库

--- SmartTrash-client/ui.py
+++ SmartTrash-client/ui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-var = tk.StringVar()
 on_hit = False
 def hit_me():
     global on_hit
@@ -10,11 +9,28 @@ def hit_me():
         on_hit = False
         var.set('')
 def init():
+    global window
+    global l
+    global b1
+    global var
     window = tk.Tk()
     window.title('My Window')
     window.geometry('500x300')
+    var = tk.StringVar()
     l = tk.Label(window, textvariable=var, bg='white', fg='black', font=('Arial', 12), width=30, height=2)
     l.pack()
-    b = tk.Button(window, text='hit me', font=('Arial', 12), width=10, height=1, command=hit_me)
-    b.pack()
+    b1 = tk.Button(window, text='hit me', font=('Arial', 12), width=10, height=1, command=hit_me)
+    b1.pack()
     window.mainloop()
+def ontake(path):
+    if window==None:
+        init()
+def yes:
+    pass
+def no:
+    pass
+def start:
+    pass
+if __name__ == '__main__':
+    init()
+    
